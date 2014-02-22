255f163b7272a5626876b16ad479816bbb5f6aa0
holyshared <holy.shared.design@gmail.com>
2014-02-22 16:13:49 +0900

    add code

--- src/CodeAnalyzer/CodeAnalyzer.php
+++ src/CodeAnalyzer/CodeAnalyzer.php
@@ -5,9 +5,19 @@ namespace CodeAnalyzer;
 class CodeAnalyzer
 {
 
+    protected static $configuration;
+
+    public static function getConfiguration()
+    {
+        if (static::$configuration === null) {
+            static::$configuration = new Configuration;
+        }
+        return static::$configuration;
+    }
+
     public static function configure(\Closure $configurator)
     {
-        $configurator(new Configuration());
+        $configurator(static::getConfiguration());
     }
 
 }
