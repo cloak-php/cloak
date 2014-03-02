a9b5c8c61bd331df5e9b608a2d1a8f124cadfa5d
holyshared <holy.shared.design@gmail.com>
2014-03-02 16:27:15 +0900

    add unittest tasks

--- Phakefile
+++ Phakefile
@@ -6,3 +6,17 @@ group('example', function() {
         require_once __DIR__ . '/example/basic_example.php';
     });
 });
+
+group('test', function() {
+
+    desc('Run unit tests');
+    task('unit', function() {
+        echo shell_exec('./vendor/bin/pho --reporter spec');
+    });
+
+    desc('Print a report of code coverage');
+    task('coverage', function() {
+        echo shell_exec('php coverage.php');
+    });
+
+});
---
+++ coverage.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once __DIR__ . "/vendor/autoload.php";
+
+use CodeAnalyzer\CodeAnalyzer;
+use CodeAnalyzer\Configuration;
+use CodeAnalyzer\Result\File;
+
+CodeAnalyzer::configure(function(Configuration $configuration) {
+
+    $configuration->collect(XDEBUG_CC_UNUSED | XDEBUG_CC_DEAD_CODE)
+        ->includeFile(function(File $file) {
+            return $file->matchPath('\/src');
+        })
+        ->excludeFile(function(File $file) {
+            return $file->matchPath('\/spec') || $file->matchPath('\/vendor');
+        });
+
+});
+
+
+$analyzer = new CodeAnalyzer();
+$analyzer->start();
+
+$defaultArgv = array('./vendor/bin/pho', '--reporter', 'spec');
+
+$argv = array_merge($defaultArgv, array(
+    'spec/ConfigurationSpec.php',
+    'spec/ResultSpec.php',
+    'spec/Result/FileSpec.php',
+    'spec/Result/LineSpec.php'
+));
+
+require_once __DIR__ . "/vendor/bin/pho";
+
+$analyzer->stop();
+
+$result = $analyzer->getResult()->getFiles();
+
+foreach ($result as $file) {
+    $result = sprintf("%s > %0.2f%% (%d/%d)",
+        $file->getPath(),
+        $file->getCodeCoverage(),
+        $file->getExecutedLineCount(),
+        $file->getExecutableLineCount()
+    );
+    echo $result . "\n";
+}
