110f3d653faf33b325e3104bfb1f8aca5621f06d
holyshared <holy.shared.design@gmail.com>
2014-03-08 19:45:10 +0900

    remove comment

--- spec/ConfigurationSpec.php
+++ spec/ConfigurationSpec.php
@@ -16,67 +16,6 @@ use CodeAnalyzer\Result\File;
 
 describe('Configuration', function() {
 
-/*
-    describe('#includeFile', function() {
-        before(function() {
-            $this->configuration = new Configuration();
-            $this->returnValue = $this->configuration->includeFile(function(File $file) {});
-        });
-        it('should return CodeAnalyzer\Configuration instance', function() {
-            expect($this->returnValue)->toEqual($this->configuration);
-        });
-    });
-
-    describe('#includeFiles', function() {
-        before(function() {
-            $filter1 = function(File $file){};
-            $filter2 = function(File $file){};
-            $this->configuration = new Configuration(); 
-            $this->returnValue = $this->configuration->includeFiles(array(
-                $filter1, $filter2
-            ));
-        });
-        it('should add filters', function() {
-            $filters = $this->configuration->includeFiles;
-            expect(count($filters))->toBe(2);
-        });
-        it('should return CodeAnalyzer\Configuration instance', function() {
-            expect($this->returnValue)->toEqual($this->configuration);
-        });
-    });
-
-    describe('#excludeFile', function() {
-        before(function() {
-            $this->configuration = new Configuration(); 
-            $this->returnValue = $this->configuration->excludeFile(function(File $file) {});
-        });
-        it('should return CodeAnalyzer\Configuration instance', function() {
-            expect($this->returnValue)->toEqual($this->configuration);
-        });
-    });
-
-    describe('#excludeFiles', function() {
-        before(function() {
-            $filter1 = function(File $file){};
-            $filter2 = function(File $file){};
-            $this->configuration = new Configuration(); 
-            $this->returnValue = $this->configuration->excludeFiles(array(
-                $filter1, $filter2
-            ));
-        });
-        it('should add filters', function() {
-            $filters = $this->configuration->excludeFiles;
-            expect(count($filters))->toBe(2);
-        });
-        it('should return CodeAnalyzer\Configuration instance', function() {
-            expect($this->returnValue)->toEqual($this->configuration);
-        });
-    });
-*/
-
-
-
-
     describe('#apply', function() {
         before(function() {
             $filter1 = function(File $file) {
@@ -94,11 +33,7 @@ describe('Configuration', function() {
             $this->configuration = $builder->includeFile($filter1)
                 ->excludeFile($filter2)
                 ->build();
-/*
-            $this->configuration = new Configuration(); 
-            $this->configuration->includeFile($filter1)
-                ->excludeFile($filter2);
-*/
+
             $this->returnValue = $this->configuration->apply($this->result);
         });
         it('should apply configuration', function() {
