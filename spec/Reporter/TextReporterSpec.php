9eeda1768047f014174a0b9f0412a4eda5f59f44
holyshared <holy.shared.design@gmail.com>
2014-03-08 21:55:28 +0900

    add text reporter

---
+++ spec/Reporter/TextReporterSpec.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file is part of CodeAnalyzer.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+use CodeAnalyzer\Result;
+use CodeAnalyzer\Result\Line;
+use CodeAnalyzer\Reporter\TextReporter;
+
+describe('TextReporter', function() {
+
+    describe('TextReporter', function() {
+        before(function() {
+            $this->result = Result::from(array(
+                'path/to/foo.php' => array(
+                    1 => Line::EXECUTED,
+                    2 => Line::EXECUTED,
+                    3 => Line::UNUSED
+                ),
+                'path/to/bar.php' => array(
+                    1 => Line::EXECUTED,
+                    2 => Line::EXECUTED,
+                    3 => Line::UNUSED
+                )
+            ));
+            $this->reporter = new TextReporter();
+        });
+        it('should output coverage', function() {
+            $output  = "";
+            $output .= "path/to/foo.php > 66.67% (2/3)" . PHP_EOL;
+            $output .= "path/to/bar.php > 66.67% (2/3)" . PHP_EOL;
+
+            expect(function() {
+                $this->reporter->stop($this->result);
+            })->toPrint($output);
+        });
+    });
+
+});
---
+++ src/Reporter/TextReporter.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This file is part of CodeAnalyzer.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace CodeAnalyzer\Reporter;
+
+use CodeAnalyzer\Result;
+
+class TextReporter {
+
+    public function stop(Result $result)
+    {
+        $files = $result->getFiles();
+
+        foreach ($files as $file) {
+            $result = sprintf("%s > %0.2f%% (%d/%d)",
+                $file->getPath(),
+                $file->getCodeCoverage(),
+                $file->getExecutedLineCount(),
+                $file->getExecutableLineCount()
+            );
+            echo $result . PHP_EOL;
+        }
+    }
+
+}
