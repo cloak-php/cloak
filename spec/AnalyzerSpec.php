8e1c356128e26489b5deeb9a875beeba987f934a
holyshared <holy.shared.design@gmail.com>
2014-03-08 16:06:07 +0900

    replace to mock

--- spec/AnalyzerSpec.php
+++ spec/AnalyzerSpec.php
@@ -2,7 +2,11 @@
 
 use CodeAnalyzer\Analyzer;
 use CodeAnalyzer\Configuration;
+use CodeAnalyzer\Result\Line;
 use CodeAnalyzer\Result\File;
+use CodeAnalyzer\Driver\DriverInterface;
+use Mockery as Mock;
+
 
 describe('Analyzer', function() {
 
@@ -14,13 +18,11 @@ describe('Analyzer', function() {
         $subject->called++;
         $subject->configuration = $configuration;
     };
-    $this->analyzer = new Analyzer(); 
-
-    before(function() {
-        Analyzer::configure($this->configurator);
-    });
 
     describe('#configure', function() {
+        before(function() {
+            Analyzer::configure($this->configurator);
+        });
         it('should called once', function() {
             expect($this->subject->called)->toBe(1);
         });
@@ -31,40 +33,53 @@ describe('Analyzer', function() {
 
     describe('#isStarted', function() {
         context('when started', function() {
+            before(function() {
+                $this->driver = Mock::mock('CodeAnalyzer\Driver\DriverInterface');
+                $this->driver->shouldReceive('start')->once();
+                $this->driver->shouldReceive('isStarted')->once()->andReturn(true);
+                $this->analyzer = new Analyzer($this->driver);
+            });
+            after(function() {
+                Mock::close();
+            });
             it('should return true', function() {
                 $this->analyzer->start();
                 expect($this->analyzer->isStarted())->toBeTrue();
             });
         });
         context('when stoped', function() {
-            it('should return false', function() {
+            before(function() {
+                $this->driver = Mock::mock('CodeAnalyzer\Driver\DriverInterface');
+                $this->driver->shouldReceive('start')->once();
+                $this->driver->shouldReceive('stop')->once();
+                $this->driver->shouldReceive('getResult')->once()->andReturn(array(
+                    'foo.php' => array( Line::EXECUTED => 1 )
+                ));
+                $this->driver->shouldReceive('isStarted')->once()->andReturn(false);
+                $this->analyzer = new Analyzer($this->driver);
                 $this->analyzer->start();
+            });
+            after(function() {
+                Mock::close();
+            });
+            it('should return false', function() {
                 $this->analyzer->stop();
                 expect($this->analyzer->isStarted())->toBeFalse();
             });
         });
     });
 
-    describe('#start', function() {
-        before(function() {
-            $this->analyzer->start();
-        });
-        it('should analyze start', function() {
-            expect($this->analyzer->isStarted())->toBeTrue();
-        });
-    });
-
-    describe('#stop', function() {
-        before(function() {
-            $this->analyzer->stop();
-        });
-        it('should analyze stop', function() {
-            expect($this->analyzer->isStarted())->toBeFalse();
-        });
-    });
-
     describe('#getResult', function() {
         before(function() {
+            $this->driver = Mock::mock('CodeAnalyzer\Driver\DriverInterface');
+            $this->driver->shouldReceive('start')->once();
+            $this->driver->shouldReceive('stop')->once();
+            $this->driver->shouldReceive('getResult')->once()->andReturn(array(
+                'src/foo.php' => array( Line::EXECUTED => 1 ),
+                'src/bar.php' => array( Line::EXECUTED => 1 ),
+                'src/vendor/foo1.php' => array( Line::EXECUTED => 1 ),
+                'src/vendor/foo2.php' => array( Line::EXECUTED => 1 )
+            ));
             Analyzer::configure(function(Configuration $configuration) {
                 $configuration->includeFile(function(File $file) {
                     return $file->matchPath('src');
@@ -72,9 +87,13 @@ describe('Analyzer', function() {
                     return $file->matchPath('vendor');
                 });
             });
+            $this->analyzer = new Analyzer($this->driver);
             $this->analyzer->start();
             $this->analyzer->stop();
             $this->result = $this->analyzer->getResult();
+        });
+        after(function() {
+            Mock::close();
         });
         it('should return an instance of CodeAnalyzer\Result', function() {
             $files = $this->result->getFiles();
