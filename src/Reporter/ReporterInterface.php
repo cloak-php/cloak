595ddd9fe9472b9a9477cd7a2cd8f33432231f63
holyshared <holy.shared.design@gmail.com>
2014-03-12 10:44:45 +0900

    add ReporterInterface

---
+++ src/Reporter/ReporterInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of CodeAnalyzer.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace CodeAnalyzer\Reporter;
+
+use Zend\EventManager\ListenerAggregateInterface;
+
+interface ReporterInterface extends ListenerAggregateInterface
+{
+}
