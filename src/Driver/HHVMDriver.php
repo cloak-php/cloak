b7c3972200ddbb46be2f4b115389c11fa3984dc2
holyshared <holy.shared.design@gmail.com>
2014-07-17 14:04:59 +0900

    add hhvm driver spec

---
+++ src/Driver/HHVMDriver.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This file is part of CodeAnalyzer.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace CodeAnalyzer\Driver;
+
+class HHVMDriver implements DriverInterface
+{
+
+    protected $started = false;
+    protected $analyzeResult = null;
+
+    public function start()
+    {
+    }
+
+    public function stop()
+    {
+    }
+
+    public function isStarted()
+    {
+    }
+
+    public function getResult()
+    {
+    }
+
+}
