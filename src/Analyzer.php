139de16e17db2840e9c00b9ff287f61dce5a070d
holyshared <holy.shared.design@gmail.com>
2014-07-27 14:48:54 +0900

    add document commnet

--- src/Analyzer.php
+++ src/Analyzer.php
@@ -22,14 +22,29 @@ class Analyzer implements NotifierAwareInterface, AnalyzeLifeCycleInterface
 
     use ProvidesNotifier;
 
-    protected $configuration = null;
-    protected $analyzeResult = null;
+    /**
+     * @var Configuration
+     */
+    protected $configuration;
 
+    /**
+     * @var Result
+     */
+    protected $analyzeResult;
+
+
+    /**
+     * @param Configuration $configuration
+     */
     public function __construct(Configuration $configuration)
     {
         $this->init($configuration);
     }
 
+    /**
+     * @param \Closure $configurator
+     * @return Analyzer
+     */
     public static function factory(\Closure $configurator)
     {
         $builder = new ConfigurationBuilder();
@@ -50,22 +65,34 @@ class Analyzer implements NotifierAwareInterface, AnalyzeLifeCycleInterface
         $this->getNotifier()->stop( $this->getResult() );
     }
 
+    /**
+     * @return bool
+     */
     public function isStarted()
     {
         return $this->driver()->isStarted();
     }
 
+    /**
+     * @return Result
+     */
     public function getResult()
     {
         $analyzeResult = $this->driver()->getResult();
-        return  $this->configuration->apply( Result::from($analyzeResult) );
+        return $this->configuration->apply( Result::from($analyzeResult) );
     }
 
+    /**
+     * @return \cloak\driver\DriverInterface
+     */
     protected function driver()
     {
         return $this->configuration->driver;
     }
 
+    /**
+     * @param Configuration $configuration
+     */
     protected function init(Configuration $configuration)
     {
         $this->configuration = $configuration;
