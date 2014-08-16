35560d9a86a82b141d8a7a835d79753d725066d1
holyshared <holy.shared.design@gmail.com>
2014-08-16 22:38:12 +0900

    add reporter

--- script/coverage.php
+++ script/coverage.php
@@ -14,12 +14,17 @@ require_once __DIR__ . "/../vendor/autoload.php";
 use cloak\Analyzer;
 use cloak\ConfigurationBuilder;
 use cloak\Result\File;
+use cloak\reporter\CompositeReporter;
+use cloak\reporter\ProcessingTimeReporter;
 use cloak\reporter\TextReporter;
 use Symfony\Component\Yaml\Yaml;
 
 $analyzer = Analyzer::factory(function(ConfigurationBuilder $builder) {
 
-    $builder->reporter(new TextReporter());
+    $builder->reporter(new CompositeReporter([
+        new TextReporter(),
+        new ProcessingTimeReporter()
+    ]));
     $builder->includeFile(function(File $file) {
         return $file->matchPath('/src');
     })->excludeFile(function(File $file) {
