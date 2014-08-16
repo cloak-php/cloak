ce48d87a5790da0851c24cac0247818946f51398
holyshared <holy.shared.design@gmail.com>
2014-08-16 22:16:37 +0900

    add reporter

---
+++ spec/reporter/ProcessingTimeReporterSpec.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of cloak.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+use cloak\Result;
+use cloak\reporter\ProcessingTimeReporter;
+use \Mockery;
+
+
+describe('ProcessingTimeReporter', function() {
+
+    describe('onStart', function() {
+        before(function() {
+            $this->reporter = new ProcessingTimeReporter();
+
+            $this->dateTime = DateTime::createFromFormat('Y-m-d H:i:s', '2014-07-01 12:00:00');
+
+            $this->startEvent = Mockery::mock('cloak\event\StartEventInterface');
+            $this->startEvent->shouldReceive('getSendAt')->andReturn( $this->dateTime );
+        });
+        it('output start datetime', function() {
+            $output = "Start at: 1 July 2014 at 12:00\n";
+
+            expect(function() {
+                $this->reporter->onStart($this->startEvent);
+            })->toPrint($output);
+        });
+        it('check mock object expectations', function() {
+            Mockery::close();
+        });
+    });
+
+});
