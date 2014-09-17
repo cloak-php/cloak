fd2b6ea4e04469c2a465119c8884dc295f874010
holyshared <holy.shared.design@gmail.com>
2014-09-17 18:33:06 +0900

    Update CI setting

--- script/coverage.php
+++ script/coverage.php
@@ -13,36 +13,37 @@ namespace cloak\script;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-
 use cloak\Analyzer;
 use cloak\ConfigurationBuilder;
 use cloak\Result\File;
 use cloak\reporter\CompositeReporter;
-use cloak\reporter\LcovReporter;
 use cloak\reporter\ProcessingTimeReporter;
 use cloak\reporter\TextReporter;
-
+use cloak\reporter\LcovReporter;
+use Symfony\Component\Yaml\Yaml;
 
 $analyzer = Analyzer::factory(function(ConfigurationBuilder $builder) {
 
-    $reporter = new CompositeReporter([
+    $builder->reporter(new CompositeReporter([
         new LcovReporter(__DIR__ . '/report.lcov'),
         new TextReporter(),
         new ProcessingTimeReporter()
-    ]);
-
+    ]));
     $builder->includeFile(function(File $file) {
         return $file->matchPath('/src');
     })->excludeFile(function(File $file) {
-        $matchExcludeTarget = $file->matchPath('/spec') || $file->matchPath('/vendor');
-        return $matchExcludeTarget;
-    })->reporter($reporter);
+        return $file->matchPath('/spec') || $file->matchPath('/vendor');
+    });
 
 });
 
 $analyzer->start();
 
-$argv = ['../vendor/bin/pho', '--stop'];
+
+$defaultArgv = array('../vendor/bin/pho');
+$coverageConfig = Yaml::parse(__DIR__ . '/../coverage.yml');
+$argv = array_merge($defaultArgv, $coverageConfig['targets']);
+
 require_once __DIR__ . "/../vendor/bin/pho";
 
 $analyzer->stop();
