ab53bfb5b53482fc90668739993886e922b3dc78
holyshared <holy.shared.design@gmail.com>
2014-09-28 16:15:20 +0900

    add MethodResult spec

--- src/result/MethodResult.php
+++ src/result/MethodResult.php
@@ -11,6 +11,126 @@
 
 namespace cloak\result;
 
-class MethodResult
+use cloak\value\LineRange;
+use cloak\NamedCoverageResultInterface;
+use cloak\value\Coverage;
+use Zend\Code\Reflection\MethodReflection;
+
+/**
+ * Class MethodResult
+ * @package cloak\result
+ */
+class MethodResult implements NamedCoverageResultInterface
 {
+
+    /**
+     * @var MethodReflection
+     */
+    private $methodReflection;
+
+    /**
+     * @var LineSet
+     */
+    private $methodLineResults;
+
+
+    /**
+     * @param MethodReflection $classReflection
+     * @param LineSetInterface $methodLineResults
+     */
+    public function __construct(MethodReflection $methodReflection, LineSetInterface $methodLineResults)
+    {
+        $lineRange = new LineRange(
+            $methodReflection->getStartLine(),
+            $methodReflection->getEndLine()
+        );
+        $rangeResults = $methodLineResults->selectRange($lineRange);
+
+        $this->methodReflection = $methodReflection;
+        $this->methodLineResults = $rangeResults;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->methodReflection->getName();
+    }
+
+    /**
+     * @return string
+     */
+    public function getNamespaceName()
+    {
+        $declaringClass = $this->methodReflection->getDeclaringClass();
+        return $declaringClass->getNamespaceName();
+    }
+
+    /**
+     * @return int
+     */
+    public function getLineCount()
+    {
+        return $this->methodLineResults->getLineCount();
+    }
+
+    /**
+     * @return int
+     */
+    public function getDeadLineCount()
+    {
+        return $this->methodLineResults->getDeadLineCount();
+    }
+
+    /**
+     * @return int
+     */
+    public function getUnusedLineCount()
+    {
+        return $this->methodLineResults->getUnusedLineCount();
+    }
+
+    /**
+     * @return int
+     */
+    public function getExecutedLineCount()
+    {
+        return $this->methodLineResults->getExecutedLineCount();
+    }
+
+    /**
+     * @return int
+     */
+    public function getExecutableLineCount()
+    {
+        return $this->methodLineResults->getExecutableLineCount();
+    }
+
+    /**
+     * @return Coverage The value of code coverage
+     */
+    public function getCodeCoverage()
+    {
+        return $this->methodLineResults->getCodeCoverage();
+    }
+
+    /**
+     * @param Coverage $coverage
+     * @return bool
+     */
+    public function isCoverageLessThan(Coverage $coverage)
+    {
+        return $this->methodLineResults->isCoverageLessThan($coverage);
+    }
+
+    /**
+     * @param Coverage $coverage
+     * @return bool
+     */
+    public function isCoverageGreaterEqual(Coverage $coverage)
+    {
+        return $this->methodLineResults->isCoverageGreaterEqual($coverage);
+    }
+
 }
