58f732b2ff6593474ad96647a3050afc7d963795
holyshared <holy.shared.design@gmail.com>
2014-10-25 18:30:52 +0900

    Update loader spec

--- src/ConfigurationLoader.php
+++ src/ConfigurationLoader.php
@@ -13,6 +13,11 @@ namespace cloak;
 
 use Yosymfony\Toml\Toml;
 use cloak\driver\result\FileResult;
+use cloak\reporter\ReporterNotFoundException;
+use cloak\reporter\CompositeReporter;
+use cloak\reporter\ReporterFactory;
+use \ReflectionClass;
+use \ReflectionException;
 
 
 /**
@@ -26,12 +31,16 @@ class ConfigurationLoader
      * @param string $configFilePath
      * @return \cloak\Configuration
      */
-    public static function loadConfigration($configFilePath)
+    public function loadConfigration($configFilePath)
     {
         $configValues = Toml::Parse($configFilePath);
+
         $defaults = $configValues['defaults'];
+        $reporter = $this->loadReporters($defaults['reporters']);
 
         $builder = new ConfigurationBuilder();
+        $builder->reporter($reporter);
+
         $builder->includeFile(function (FileResult $file) {
             return $file->matchPaths($defaults['includes']);
         })->excludeFile(function (FileResult $file) {
@@ -39,6 +48,67 @@ class ConfigurationLoader
         });
 
         return $builder->build();
+    }
+
+
+    /**
+     * @param array $config
+     */
+    private function loadReporters(array $config)
+    {
+
+        $reporters = [];
+        $reporterNames = $config['uses'];
+        $reporterConfigs = $config['config'];
+
+        foreach ($reporterNames as $reporterName) {
+            $args = $reporterConfigs[$reporterName];
+            $reporters[] = $this->loadReporter($reporterName, $args);
+        }
+
+        return new CompositeReporter($reporters);
+    }
+
+    /**
+     * @param string $reporterName
+     * @param array $arguments
+     * @return \cloak\reporter\ReporterInterface
+     */
+    private function loadReporter($reporterName, array $arguments = [])
+    {
+        $reporterClassNameWithNamespace = $this->getReporterFullName($reporterName);
+
+        try {
+            $reflection = new ReflectionClass($reporterClassNameWithNamespace);
+        } catch (ReflectionException $exception) {
+            throw new ReporterNotFoundException($exception);
+        }
+
+        $factory = new ReporterFactory($reflection);
+
+        return $factory->createWithArguments($arguments);
+    }
+
+    private function getReporterFullName($reporterName)
+    {
+        $reporterClassName = ucfirst($reporterName) . 'Reporter';
+        $reporterClassNameWithNamespace = "cloak\\reporter\\{$reporterClassName}";
+
+        return $reporterClassNameWithNamespace;
+    }
+
+
+    private function getReporterReflection($reporterName)
+    {
+        $reporterClassNameWithNamespace = $this->getReporterFullName($reporterName);
+
+        try {
+            $reflection = new ReflectionClass($reporterClassNameWithNamespace);
+        } catch (ReflectionException $exception) {
+            throw new ReporterNotFoundException($exception);
+        }
+
+        return $reflection;
     }
 
 }
