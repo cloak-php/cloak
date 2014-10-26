78cc11f203b07c0037fd763c6148565deeef4076
holyshared <holy.shared.design@gmail.com>
2014-10-26 20:01:09 +0900

    Update loader

--- src/ConfigurationLoader.php
+++ src/ConfigurationLoader.php
@@ -12,10 +12,7 @@
 namespace cloak;
 
 use Yosymfony\Toml\Toml;
-use cloak\driver\result\FileResult;
-use cloak\reporter\CompositeReporter;
-use cloak\reporter\ReporterFactory;
-use Zend\Config\Config;
+use cloak\configuration\Root;
 
 
 /**
@@ -26,58 +23,17 @@ class ConfigurationLoader
 {
 
     /**
-     * @var \Zend\Config\Config
-     */
-    private $config;
-
-
-    /**
      * @param string $configFilePath
      * @return \cloak\Configuration
      */
     public function loadConfigration($configFilePath)
     {
         $configValues = Toml::Parse($configFilePath);
-        $this->config = new Config($configValues);
 
-        $reporters = $this->config->get('reporters');
-        $reporter = $this->loadReporters($reporters);
-
-        $builder = new ConfigurationBuilder();
-        $builder->reporter($reporter);
-
-        $includes = $this->config->get('includes', new Config([]));
-        $excludes = $this->config->get('excludes', new Config([]));
-
-        $builder->includeFile(function (FileResult $file) use($includes) {
-            return $file->matchPaths($includes->toArray());
-        })->excludeFile(function (FileResult $file) use($excludes) {
-            return $file->matchPaths($excludes->toArray());
-        });
+        $root = new Root($configValues);
+        $builder = $root->applyTo(new ConfigurationBuilder());
 
         return $builder->build();
-    }
-
-
-    /**
-     * @param \Zend\Config\Config
-     */
-    private function loadReporters(Config $config)
-    {
-        $reporters = [];
-        $reporterNames = $config->get('uses');
-        $reporterConfigs = $config->get('configs');
-
-        foreach ($reporterNames as $reporterName) {
-            $arguments = $reporterConfigs->get($reporterName, new Config([]));
-
-            $factory = ReporterFactory::fromName($reporterName);
-            $reporter = $factory->createWithArguments( $arguments->toArray() );
-
-            $reporters[] = $reporter;
-        }
-
-        return new CompositeReporter($reporters);
     }
 
 }
