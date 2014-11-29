6e484c48d9eb4e9e048a905921fc105b56c376d0
holyshared <holy.shared.design@gmail.com>
2014-11-29 17:43:26 +0900

    Update text report spec

--- spec/reporter/TextReporter.spec.php
+++ spec/reporter/TextReporter.spec.php
@@ -14,8 +14,6 @@ use cloak\result\LineResult;
 use cloak\reporter\TextReporter;
 use cloak\driver\Result as AnalyzeResult;
 use cloak\event\StopEvent;
-use Zend\Console\Console;
-use Zend\Console\ColorInterface as Color;
 use \Mockery;
 
 
@@ -43,64 +41,37 @@ describe('TextReporter', function() {
 
     describe('onStop', function() {
         beforeEach(function() {
-            $this->console = Console::getInstance();
+            $expectResultFile = __DIR__ . '/../fixtures/report/text_report.log';
+            $this->expectResult = file_get_contents($expectResultFile);
+
+            $sourceFile1 = realpath(__DIR__ . '/../fixtures/report/src/Example1.php');
+            $sourceFile2 = realpath(__DIR__ . '/../fixtures/report/src/Example2.php');
+            $sourceFile3 = realpath(__DIR__ . '/../fixtures/report/src/Example3.php');
 
             $coverages = [
-                realpath(__DIR__ . '/../../src/driver/XdebugDriver.php') => [
-                    1 => LineResult::EXECUTED,
-                    2 => LineResult::EXECUTED,
-                    3 => LineResult::UNUSED
+                $sourceFile1 => [
+                    13 => LineResult::EXECUTED,
+                    18 => LineResult::EXECUTED
                 ],
-                realpath(__DIR__ . '/../../src/result/LineResult.php') => [
-                    1 => LineResult::EXECUTED,
-                    2 => LineResult::EXECUTED,
-                    3 => LineResult::EXECUTED,
-                    4 => LineResult::EXECUTED,
-                    5 => LineResult::EXECUTED,
-                    6 => LineResult::EXECUTED,
-                    7 => LineResult::EXECUTED,
-                    8 => LineResult::UNUSED,
-                    9 => LineResult::UNUSED,
-                    10 => LineResult::UNUSED
+                $sourceFile2 => [
+                    13 => LineResult::EXECUTED,
+                    18 => LineResult::UNUSED
                 ],
-                realpath(__DIR__ . '/../../src/result/FileResult.php') => [
-                    1 => LineResult::EXECUTED,
-                    2 => LineResult::EXECUTED,
-                    3 => LineResult::UNUSED,
-                    4 => LineResult::UNUSED,
-                    5 => LineResult::UNUSED,
-                    6 => LineResult::UNUSED,
-                    7 => LineResult::UNUSED
+                $sourceFile3 => [
+                    13 => LineResult::UNUSED,
+                    18 => LineResult::UNUSED
                 ]
             ];
 
             $analyzeResult = AnalyzeResult::fromArray($coverages);
             $this->stopEvent = new StopEvent(Result::fromAnalyzeResult($analyzeResult));
 
-            $totalCoverage = sprintf('%6.2f%%', 55.00);
-            $this->totalCoverage = $this->console->colorize($totalCoverage, Color::NORMAL);
-
-            $highCoverage = sprintf('%6.2f%%', (float) 70);
-            $this->high = $this->console->colorize($highCoverage, Color::GREEN) . ' ';
-
-            $lowCoverage = sprintf('%6.2f%%', (float) 28.57);
-            $this->low = $this->console->colorize($lowCoverage, Color::YELLOW) . ' ';
-
-            $normalCoverage = sprintf('%6.2f%%', (float) 66.67);
-            $this->normal = $this->console->colorize($normalCoverage, Color::NORMAL) . ' ';
-
             $this->reporter = new TextReporter();
         });
-        it('output coverage', function() {
-            $output  = "";
-            $output .= $this->normal . "( 2/ 3) src/driver/XdebugDriver.php" . PHP_EOL;
-            $output .= $this->high . "( 7/10) src/result/LineResult.php" . PHP_EOL;
-            $output .= $this->low . "( 2/ 7) src/result/FileResult.php" . PHP_EOL;
-            $output .= PHP_EOL . "Code Coverage:" . $this->totalCoverage . PHP_EOL;
-
+        it('output text report', function() {
             expect(function() {
                 $this->reporter->onStop($this->stopEvent);
-            })->toPrint($output);
+            })->toPrint($this->expectResult);
         });
     });
 });
