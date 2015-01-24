783aa50fd1635ed20ea94608763357ea56c067c0
Noritaka Horio <holy.shared.design@gmail.com>
2015-01-24 21:42:29 +0900

    Upgrade robo

--- RoboFile.php
+++ RoboFile.php
@@ -1,8 +1,9 @@
 <?php
 
 use \Robo\Tasks;
-use \coverallskit\robo\CoverallsKitTasks;
-use \peridot\robo\PeridotTasks;
+use \coverallskit\robo\loadTasks as CoverallsKitTasks;
+use \peridot\robo\loadTasks as PeridotTasks;
+
 
 /**
  * Class RoboFile
@@ -37,7 +38,7 @@ class RoboFile extends Tasks
     public function coverallsUpload()
     {
         $result = $this->taskCoverallsKit()
-            ->configure('coveralls.toml')
+            ->configureBy('coveralls.toml')
             ->run();
 
         return $result;
