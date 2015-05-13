c001bf87be74039a0cfac4f85bd02308f67eb17d
holyshared <holy.shared.design@gmail.com>
2015-05-13 09:28:01 +0900

    fix https://scrutinizer-ci.com/g/cloak-php/cloak/issues/dev-2.0.0/files/src/reflection/ClassReflection.php?selectedLabels%5B0%5D=9&orderField=path&order=asc

--- src/reflection/ClassReflection.php
+++ src/reflection/ClassReflection.php
@@ -60,7 +60,7 @@ class ClassReflection implements Reflection, ResultConvertible
      */
     public function getName()
     {
-        return $this->reflection->getName();
+        return $this->reflection->name;
     }
 
     /**
@@ -103,7 +103,7 @@ class ClassReflection implements Reflection, ResultConvertible
      */
     public function getMethods()
     {
-        $className = $this->reflection->getName();
+        $className = $this->reflection->name;
         $selector = MethodSelector::fromClassName($className);
 
         $result = $selector->excludeNative()
